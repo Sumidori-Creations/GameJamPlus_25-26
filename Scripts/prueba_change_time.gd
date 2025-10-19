e96ac0cc4cd49367b3a98d1f72281d7a688ba415
IssaAC00 <paola.alguera@estudiantec.cr>
2025-10-19 10:34:57 -0600

    PUZZLEa1

--- Scripts/prueba_change_time.gd
+++ Scripts/prueba_change_time.gd
@@ -6,20 +6,11 @@ var pastTime : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for triggerWall in triggerWallContainer.get_children():
-		triggerWall.cambiazo.connect(changeTime)
+	##for triggerWall in triggerWallContainer.get_children():
+	##	triggerWall.cambiazo.connect(changeTime)
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func changeTime(triggerWall: Node) -> void:
-	print(triggerWall.name)
-	triggerWall.activo = !triggerWall.activo
-	if triggerWall.activo:
-		triggerWall.sprite.visible = false
-		triggerWall.collision_layer = 0		
-	else:
-		triggerWall.sprite.visible = true
-		triggerWall.collision_layer = 3		
+	pass	

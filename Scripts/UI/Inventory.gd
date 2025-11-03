95f6e2acc86d3985eb8c78321ee357799c5d07c2
David Pastor <xdavidpastor@gmail.com>
2025-11-02 22:11:12 -0600

    Assets for Inventory

---
+++ Scripts/UI/Inventory.gd
@@ -0,0 +1,17 @@
+extends Node
+
+#Esta vara funciona Así:
+#	Este mae tiene N espacios, el sabe que hay en cada espacio, cuantos tiene ocupados y cuantos disponibles
+#	Envía y recibe señales, guardeme esto, deme aquello en el espacio N
+#	Fuera de aquí nadie sabe nada del inventario
+#	Se guarda en una estructura de casilla de inventario
+#	Esta tiene, su posición, su estado, su contenido
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

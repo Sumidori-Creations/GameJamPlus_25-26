988763dfb7a05ec05f8b79bc5eab593f66350350
David Pastor <xdavidpastor@gmail.com>
2025-12-12 21:09:36 -0600

    La cigueña cigueña si, la cigueña cigueña no

--- Scripts/credits.gd
+++ Scripts/credits.gd
@@ -2,5 +2,19 @@ extends Control
 
 signal volver
 
+@onready var okButton = $ColorRect2/Button
+
+func _ready() -> void:
+	okButton.focus_neighbor_top = NodePath(".")
+	okButton.focus_neighbor_bottom = NodePath(".")
+	okButton.focus_neighbor_left = NodePath(".")
+	okButton.focus_neighbor_right = NodePath(".")
+
 func _on_button_pressed() -> void:
 	volver.emit()
+
+func _on_visibility_changed() -> void:
+	if self.visible:
+		okButton.grab_focus()
+	else:
+		pass
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,24 @@
 extends Control
 
+@onready var playButton = $exitButton
+@onready var exitButton = $playButton
+@onready var creditsButton = $creditsButton
+
 func _ready() -> void:
 	$Credits.volver.connect(_close_credits)
+	playButton.focus_mode = Control.FOCUS_ALL
+	exitButton.focus_mode = Control.FOCUS_ALL
+	creditsButton.focus_mode = Control.FOCUS_ALL
+	playButton.focus_neighbor_bottom = creditsButton.get_path()
+	playButton.focus_neighbor_right = creditsButton.get_path()
+	playButton.focus_neighbor_left = exitButton.get_path()
+	creditsButton.focus_neighbor_top = playButton.get_path()
+	creditsButton.focus_neighbor_left = playButton.get_path()
+	creditsButton.focus_neighbor_right = exitButton.get_path()
+	exitButton.focus_neighbor_top = playButton.get_path()
+	exitButton.focus_neighbor_right = playButton.get_path()
+	exitButton.focus_neighbor_left = creditsButton.get_path()
+	playButton.grab_focus()
 	
 func _on_exit_button_pressed() -> void:
 	$AudioStreamPlayer.stop()
@@ -15,3 +32,6 @@ func _on_credits_button_pressed() -> void:
 
 func _close_credits() -> void:
 	$Credits.visible = false
+
+func _on_credits_volver() -> void:
+	playButton.grab_focus()
